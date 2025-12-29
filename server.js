39dcc7400b1a71b07c026ea69aa4ffbd20582e81
mimi489 <nnnrrrtttsss123@gmail.com>
2025-12-29 14:07:57 +0100

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://soccer-2025.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
