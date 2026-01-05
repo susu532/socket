2aa3d7713e8ead524dfc72ec63b64374fca21a91
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-05 13:19:04 +0100

    update

--- rooms/SoccerRoom.js
+++ rooms/SoccerRoom.js
@@ -112,6 +112,7 @@ export class SoccerRoom extends Room {
       // halfX=1 (2m thick), halfY=5 (10m high), halfZ=5 (10m wide to overlap sides)
       const desc = RAPIER.ColliderDesc.cuboid(1, 5, 5)
         .setTranslation(x, 5, z)
+        .setRestitution(1.2)
       this.world.createCollider(desc)
     })
 
@@ -152,6 +153,7 @@ export class SoccerRoom extends Room {
       // halfX=2.5 (5m deep), halfY=5 (10m high), halfZ=1 (2m thick)
       const desc = RAPIER.ColliderDesc.cuboid(2.5, 5, 1)
         .setTranslation(x, 5, z)
+        .setRestitution(1.2)
       this.world.createCollider(desc)
     })
 
