2aa5bd958954e18bff350a0c490ae4cdbe37d352
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-07 13:13:38 +0100

    update

--- rooms/SoccerRoom.js
+++ rooms/SoccerRoom.js
@@ -249,7 +249,7 @@ export class SoccerRoom extends Room {
 
     const body = this.world.createRigidBody(bodyDesc)
 
-    const collider = RAPIER.ColliderDesc.cuboid(0.2, 0.4, 0.2)
+    const collider = RAPIER.ColliderDesc.cuboid(0.4, 0.4, 0.4)
       .setTranslation(0, 0.4, 0)
       .setFriction(2.0)
       .setRestitution(0.0)
