2f6b9240b35a9f46afd37b292fc04d0d89375bdc
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-08 15:05:00 +0100

    update

--- rooms/SoccerRoom.js
+++ rooms/SoccerRoom.js
@@ -390,7 +390,7 @@ export class SoccerRoom extends Room {
     const dz = ballPos.z - playerPos.z
     const dist = Math.sqrt(dx * dx + dy * dy + dz * dz)
 
-    if (dist < 2.2) {
+    if (dist < 1.7) {
       const { impulseX, impulseY, impulseZ } = data
       const kickMult = player.kickMult || 1
 
