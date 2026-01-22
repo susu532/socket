b6fe814d3cefc49ece70847f17cca9cc0000903d
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-22 19:19:28 +0100

    update

--- schema/GameState.js
+++ schema/GameState.js
@@ -19,16 +19,6 @@ export class PlayerState extends Schema {
     this.jumpCount = 0
     this.sessionId = ''
     
-    // Server-only input state (not synced)
-    this.inputX = 0
-    this.inputZ = 0
-    this.inputJump = false
-    this.inputRotY = 0
-    this.prevJump = false
-    
-    // Logic flags
-    this.resetPosition = false
-    
     // Power-up multipliers
     this.speedMult = 1
     this.jumpMult = 1
@@ -149,3 +139,4 @@ defineTypes(GameState, {
   currentTick: 'number',
   countdownTimer: 'number'
 })
+
