69e488d1df19ac27680eecde558ff26901a9a1ea
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-23 17:00:01 +0100

    update

--- schema/PhysicsConstants.js
+++ schema/PhysicsConstants.js
@@ -28,7 +28,7 @@ PLAYER_RADIUS: 1.2,
   // Restitution
   GROUND_RESTITUTION: 0.6,
   WALL_RESTITUTION: 0.3,
-  GOAL_RESTITUTION: 1.5,  // Increased for stronger net bounce
+  GOAL_RESTITUTION: 2.0,  // Increased for stronger net bounce
   POST_RESTITUTION: 0.3,
   
   // Wall Dimensions
